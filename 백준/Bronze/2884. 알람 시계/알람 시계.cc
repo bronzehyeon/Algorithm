fa28544cb970dafc2a71e8a5379fcf2f8728b6aa
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-01-26 16:16:05 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int H, M;
+	cin >> H >> M;
+	if (M < 45) {
+		if (H == 0) H = 23;
+		else H -= 1;
+		M = 60 - (45 - M);
+	}
+	else M -= 45;
+	cout << H << " " << M;
+}
