a0266d9c8e2c2059a316cd6803ce0bc7ba2e565b
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-01-28 16:46:18 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int x, y, w, h;
+    cin >> x >> y >> w >> h;
+    int result = min({ x, y, w - x, h - y });
+    cout << result;
+}
