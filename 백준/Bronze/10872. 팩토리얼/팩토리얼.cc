e57ec2e6ecdcbb114f432e4df5775d915355d8d4
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-02-08 21:32:13 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int dataStructure[100000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int answer = 1;
+
+    for (int i = 1; i <= N; i++) {
+        answer *= i;
+    }
+
+    cout << answer;
+}
