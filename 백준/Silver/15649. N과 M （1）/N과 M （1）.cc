d86a7542a324aee952567629ed8c479c10e95976
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-02-17 21:19:51 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[9];
+bool visited[9];
+
+void dfs(int count, int N, int M) {
+    if (count == M) {
+        for (int i = 0; i < M; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            arr[count] = i;
+            dfs(count + 1, N, M);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    dfs(0, N, M);
+}
