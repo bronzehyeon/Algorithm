62bec145f5d954b962c7dc378405f09e8ac69e8e
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-03-02 20:45:49 +0900

    [Gold III] Title: 나머지 합, Time: 132 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<long long int> rest;
+
+    for (int i = 0; i < M; i++) {
+        rest.push_back(0);
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < N; i++) {
+        int num;
+        cin >> num;
+
+        sum = (sum + num) % M;
+
+        rest[sum]++;
+    }
+
+    long long int answer = rest[0];
+
+
+    for (int i = 0; i < M; i++) {
+        answer += rest[i] * (rest[i] - 1) / 2;
+    }
+
+
+    cout << answer;
+
+}
