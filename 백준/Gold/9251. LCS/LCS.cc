f4d386e99c1c3bbc1073baba0c9a12002bac22f6
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-03-14 21:04:38 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int dp[1001][1001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string A, B;
+    cin >> A >> B;
+
+    for (int i = 1; i <= A.length(); i++) {
+        for (int j = 1; j <= B.length(); j++) {
+            if (A[i - 1] == B[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+
+    cout << dp[A.length()][B.length()];   
+}
