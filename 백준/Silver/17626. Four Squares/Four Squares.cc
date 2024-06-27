558f715eae73d1c06ea87d6dbafa3714629d000e
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-06-27 11:08:18 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int dp[50001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		for (int j = 1; j * j <= i; j++) {
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+
+	cout << dp[N];
+	
+}
