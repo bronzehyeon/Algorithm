3f1d1c72609776ba279a08362a489e1f344c4967
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-08-12 12:09:04 +0900

    [Gold III] Title: 치즈, Time: 16 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+
+int N, M;
+int arr[100][100];
+bool visited[100][100];
+bool visitedC[100][100];
+
+int dxy[2][4] = { {1, -1, 0, 0}, {0, 0, 1, -1} };
+
+void air(int x, int y) {
+	queue<pair<int, int>> q;
+	visited[x][y] = true;
+	q.push({ x, y });
+
+	while (!q.empty()) {
+		int curx = q.front().first;
+		int cury = q.front().second;
+		q.pop();
+
+		//cout << curx << " " << cury << '\n';
+
+		arr[curx][cury] = 2;
+
+		for (int i = 0; i < 4; i++) {
+			int nx = curx + dxy[0][i];
+			int ny = cury + dxy[1][i];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+				continue;
+			}
+
+			if (!visited[nx][ny] && arr[nx][ny] != 1) {
+				visited[nx][ny] = true;
+				q.push({ nx, ny });
+			}
+		}
+		
+	}
+}
+
+bool isEnd() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[i][j] == 1) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool melt(int x, int y) {
+	int cnt = 0;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dxy[0][i];
+		int ny = y + dxy[1][i];
+
+		if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+			continue;
+		}
+		if (arr[nx][ny] == 2) {
+			cnt++;
+		}
+	}
+	if (cnt >= 2) {
+		return true;
+	}
+	else {
+		return false;
+	}
+	
+}
+
+void hour() {
+	memset(visited, false, sizeof(visited));
+	air(0, 0);
+	air(0, M - 1);
+	air(N - 1, 0);
+	air(N - 1, M - 1);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (arr[i][j] == 1) {
+				if (melt(i, j)) {
+					arr[i][j] = 0;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	int ans = 0;
+
+	while (!isEnd()) {
+		hour();
+		ans++;
+		//cout << ans << '\n';
+	}
+
+	cout << ans;
+}
+
